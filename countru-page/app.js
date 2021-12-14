d2afde9148d780efac99d653b4385aaccfcf8a34
mubosher-dev <88577381+mubosher-dev@users.noreply.github.com>
2021-12-14 20:47:35 +0500

    Update app.js

--- countru-page/app.js
+++ countru-page/app.js
@@ -3,7 +3,9 @@ window.addEventListener("load", function(){
     let select = document.querySelector('.select');
     let country_wrapper = document.querySelector('.country-wrapper');
     const COUNTRY_API = "https://restcountries.com/v2/all";
-
+    let wrapper = document.querySelector('.wrapper-item');
+    let cancelBtn = document.querySelector('.cancel-btn');
+    let checked ;
     function getCountries(api){
         fetch(api)
         .then(response => {
@@ -23,7 +25,7 @@ window.addEventListener("load", function(){
 
             item.innerHTML = `
             <div class="country-img">
-                <img src="${data.flag}" alt="">
+                <img src="${data.flag}" alt="${data.name}">
              </div>
             <div class="country-description">
                 <h1 class="countryName">Davlat nomi: ${data.name}</h1>
@@ -31,26 +33,22 @@ window.addEventListener("load", function(){
                 <p>Qita: ${data.region}</p>
                 <p class="capital">Poytaxti: ${data.capital}</p>
             </div>
+            
             `;
             country_wrapper.appendChild(item);
+            wrapperFunct(item,data,wrapper)
         });
     }
-
-    input.addEventListener('keyup', function(){
-      btn(input);
-    });
-
-    select.addEventListener('change', function(e){
-        btn(select)
-    });
-
+    
     function btn(a){
         let value = a.value;
         if(value === "all") {
             country_wrapper.innerHTML = "";
             getCountries(COUNTRY_API);
+            
         }
         else{
+            checked = false;
             let items = country_wrapper.querySelectorAll('.country-item');
             items.forEach(item => {
                 if(item.innerText.toLowerCase().indexOf(value) > -1){
@@ -61,4 +59,52 @@ window.addEventListener("load", function(){
             });
         }
     }
+
+    input.addEventListener('keyup', function(){
+      btn(input);
+      if(checked === false){
+          window.location.reload();
+      }
+    });
+
+    select.addEventListener('change', function(e){
+        btn(select)
+    });
+
+
+    function wrapperFunct(item,data,wrapper){
+            item.addEventListener("click", function(){
+                wrapper.classList.add("active");
+                let div = document.createElement('div');
+                let divtwo = document.createElement('div');
+                div.classList.add('wrapper-info');
+                divtwo.classList.add('country-imgs');
+                
+                div.innerHTML = `
+                <h1 class="count">Davlat nomi : ${data.name}</h1>
+                <h2>Aholisi: ${data.population}</h2>
+                <h2>Poytaxti: ${data.capital}</h2>
+                <h2>Xaqiqiy nomi: ${data.nativeName}</h2>
+                <h2>Telefon kodi: +${data.callingCodes}</h2>
+                `;
+                divtwo.innerHTML = `
+                    <img src="${data.flag}" alt="${data.name}">
+                `;
+                
+                wrapper.appendChild(div);
+                wrapper.appendChild(divtwo);
+
+                removeChild(cancelBtn,wrapper,div,divtwo);
+            });
+           
+    }
+
+    function removeChild(item,wrapper,fi,third){
+        item.addEventListener("click", function(){
+            wrapper.classList.remove("active");
+            wrapper.removeChild(fi);
+            wrapper.removeChild(third);
+        });
+    }
+  
 })
